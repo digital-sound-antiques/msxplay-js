b11777b620936ca7cc32dfdc4692840d36de7706
Mitsutaka Okazaki <okaxaki@users.noreply.github.com>
2017-11-19 02:12:31 +0900

    Disable RC filter.

--- src/msxplay.js
+++ src/msxplay.js
@@ -32,6 +32,7 @@ module.exports = (function() {
 		this.scriptNodeDestination = this.gainNode;
 
 		this.kssplay = new KSSPlay(this.sampleRate);
+		this.kssplay.setRCF(0,0);
 		this.kssplay.setSilentLimit(3000);
 		this.kssplay.setDeviceQuality({'psg':1,'scc':0,'opll':1,'opl':1});
 		this.kss = null;
