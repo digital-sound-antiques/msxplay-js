fa13655b8cbb7bf0671dd2a68d7c1cb34c723270
Mitsutaka Okazaki <okaxaki@users.noreply.github.com>
2017-11-19 22:44:55 +0900

    Resolve security error on firefox.

--- js/lang-switcher.js
+++ js/lang-switcher.js
@@ -1,10 +1,4 @@
 (function(){
-	var style = document.createElement('style');
-	style.type = "text/css";
-	document.getElementsByTagName('head')[0].appendChild(style);
-	css = document.styleSheets[0];
-	css.insertRule(".en *:lang(ja){display:none;}",0);
-	css.insertRule(".ja *:lang(en){display:none;}",0);
 	var lang = (navigator.browserLanguage || navigator.language || navigator.userLanguage);
 
 	window.addEventListener('DOMContentLoaded', function(){
