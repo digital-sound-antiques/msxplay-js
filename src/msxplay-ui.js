b375ae3429175cdc5d5c63cda08c834c6dd3f1d8
Mitsutaka Okazaki <okaxaki@users.noreply.github.com>
2020-07-20 07:52:01 +0900

    Unmute again after browser is back from background.

--- src/msxplay-ui.js
+++ src/msxplay-ui.js
@@ -32,7 +32,7 @@ class MSXPlayUI {
   constructor() {
     this.msxplay = new MSXPlay();
     this.playerElements = [];
-    this.unmuteTried = false;
+    this._audioTag = null;
     setInterval(this.updateDisplay.bind(this), 100);
   }
 
@@ -225,18 +225,18 @@ class MSXPlayUI {
   // unmute for mobile browsers
   // https://stackoverflow.com/questions/21122418/ios-webaudio-only-works-on-headphones/46839941#46839941
   unmute() {
-    if (this.unmuteTried) {
-      return;
+    if (this._audioTag) {
+      // dispose audio tag.
+      this._audioTag.removeAttribute('src');
     }
     const silenceDataURL = "data:audio/mp3;base64,//MkxAAHiAICWABElBeKPL/RANb2w+yiT1g/gTok//lP/W/l3h8QO/OCdCqCW2Cw//MkxAQHkAIWUAhEmAQXWUOFW2dxPu//9mr60ElY5sseQ+xxesmHKtZr7bsqqX2L//MkxAgFwAYiQAhEAC2hq22d3///9FTV6tA36JdgBJoOGgc+7qvqej5Zu7/7uI9l//MkxBQHAAYi8AhEAO193vt9KGOq+6qcT7hhfN5FTInmwk8RkqKImTM55pRQHQSq//MkxBsGkgoIAABHhTACIJLf99nVI///yuW1uBqWfEu7CgNPWGpUadBmZ////4sL//MkxCMHMAH9iABEmAsKioqKigsLCwtVTEFNRTMuOTkuNVVVVVVVVVVVVVVVVVVV//MkxCkECAUYCAAAAFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV";
-    const tag = document.createElement("audio");
+    const tag = document.createElement('audio');
     tag.controls = false;
     tag.preload = "auto";
     tag.loop = false;
     tag.src = silenceDataURL;
-    const p = tag.play();
-    if (p) p.then(function () { console.log("play success") }, function (reason) { console.log("play failed", reason) });
-    this.unmuteTried = true;
+    tag.play();
+    this._audioTag = tag;
   }
 
   play(playerElement) {
